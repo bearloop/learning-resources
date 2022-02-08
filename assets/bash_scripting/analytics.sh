bf45280822f4867d6bf4cedbd8af83a8f5c0367c
bearloop <55312383+bearloop@users.noreply.github.com>
2022-02-08 22:03:01 +0200

    Bash assets

---
+++ assets/bash_scripting/analytics.sh
@@ -0,0 +1,15 @@
+#!/bin//bash
+
+# Create a function
+function get_number_wins () {
+
+  # Filter aggregate results by argument
+  win_stats=$(cat assets/bash_scripting/soccer_scores.csv | cut -d "," -f2 | egrep -v 'Winner'| sort | uniq -c | egrep "$1")
+
+}
+
+# Call the function with specified argument
+get_number_wins "Etar"
+
+# Print out the global variable
+echo "The aggregated stats are: $win_stats"
